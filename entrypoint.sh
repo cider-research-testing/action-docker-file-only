e3d5e7cc661fa002f50c23ba289d2662dd46a51e
yaron-cider <100203813+yaron-cider@users.noreply.github.com>
2023-02-27 15:23:16 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ curl --unix-socket /var/run/docker.sock http:/1.34/containers/json
 curl \
     -s \
     --unix-socket /var/run/docker.sock \
-    "http:/containers/create?name=nice" \
+    "http:/1.3.4/containers/create?name=nice" \
     -X POST \
     -H "Content-Type: application/json" \
     -d '{ "Image": "ubuntu:latest", "Cmd": [ "id" ] }'
