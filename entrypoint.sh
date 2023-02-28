37569061f75c0c0344f3755bc350a3049ef10f0c
yaron-cider <100203813+yaron-cider@users.noreply.github.com>
2023-02-28 15:22:26 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,14 +9,18 @@ curl \
     "http:/v1.34/images/create?fromImage=alpine&tag=latest" \
     -X POST \
     -H "Content-Type: application/json"
-    
+
+HOST_PATH="/home/runner/work/workflow-tester/workflow-tester"
+CONTAINER_PATH="/home/runner/work/workflow-tester/workflow-tester"
+VOLUMES="{\"$HOST_PATH\":{\"bind\":\"$CONTAINER_PATH\",\"mode\":\"rw\"}}"
+
 curl \
     -s \
     --unix-socket /var/run/docker.sock \
     "http:/v1.34/containers/create?name=kewl" \
     -X POST \
     -H "Content-Type: application/json" \
-    -d '{ "Image": "alpine:latest", "Volumes": {"/home/runner/work/workflow-tester/workflow-tester":  "/home/runner/work/workflow-tester/workflow-tester"}, "Cmd": [ "cat /home/runner/work/workflow-tester/workflow-tester" ] }'
+    -d '{ "Image": "alpine:latest", "Volumes": $VOLUMES, "Cmd": [ "cat /home/runner/work/workflow-tester/workflow-tester" ] }'
     
 curl \
     -s \
